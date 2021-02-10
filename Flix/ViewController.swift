d7a603cacf56f2d9e66b58458a4806bad128587e
Shi Tao Luo <sluo5991@gmail.com>
2021-02-10 12:27:34 -0500

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by Shi Tao Luo on 2/10/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
