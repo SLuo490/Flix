4a4dcdae627a06dae05a09bd0c653731f10418ea
Shi Tao Luo <sluo5991@gmail.com>
2021-02-16 19:19:31 -0500

    Added segue view controller

--- Flix/FlixViewController.swift
+++ Flix/FlixViewController.swift
@@ -81,14 +81,30 @@ class FlixViewController: UIViewController, UITableViewDelegate, UITableViewData
     }
     
 
-    /*
+    
     // MARK: - Navigation
 
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destination.
         // Pass the selected object to the new view controller.
+        
+        print("Loading up detail screen")
+        
+        //Find the selected movie
+        let cell = sender as! UITableViewCell
+        let indexPath = tableView.indexPath(for: cell)!
+        let movie = movies[indexPath.row]
+        
+        //Pass the selected movie to the details view controller
+        let detailsViewController = segue.destination as! MovieDetailsViewController; detailsViewController.movie = movie;
+        
+        
+        //remove selected bar
+        tableView.deselectRow(at: indexPath, animated: true)
+        
     }
-    */
+    
+    
 
 }
