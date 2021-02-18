871389a750f9b7dbad055948fe39cad64aad3ba2
Shi Tao Luo <sluo5991@gmail.com>
2021-02-18 14:40:18 -0500

    Implemented collection view layout

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Shi Tao Luo on 2/18/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
--- Flix/MovieGridViewController.swift
+++ Flix/MovieGridViewController.swift
@@ -6,14 +6,32 @@
 //
 
 import UIKit
+import AlamofireImage
 
-class MovieGridViewController: UIViewController {
+class MovieGridViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
 
+    @IBOutlet var collectionView: UICollectionView!
+    
     var movies = [[String:Any]]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "https://api.themoviedb.org/3/movie/{movie_id}/similar?api_key=<<api_key>>")!
+        
+        collectionView.delegate = self
+        collectionView.dataSource = self
+        
+        //add layout to collection view
+        let layout = collectionView.collectionViewLayout as! UICollectionViewFlowLayout
+        
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        
+        let width = (view.frame.size.width - layout.minimumLineSpacing * 2)/3
+        layout.itemSize = CGSize(width: width, height: width * 1.5)
+        
+        
+        
+        let url = URL(string: "https://api.themoviedb.org/3/movie/297762/similar?api_key=a07e22bc18f5cb106bfe4cc1f83ad8ed")!
         let request = URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 10)
         let session = URLSession(configuration: .default, delegate: nil, delegateQueue: OperationQueue.main)
         let task = session.dataTask(with: request) { (data, response, error) in
@@ -25,18 +43,34 @@ class MovieGridViewController: UIViewController {
                 JSONSerialization.jsonObject(with: data, options: []) as! [String: Any]
  
             self.movies = dataDictionary["results"] as! [[String:Any]]
-            print(self.movies)
-            
-            print(self.movies)
-        
+
+            self.collectionView.reloadData()
            }
             
         }
         task.resume()
-    
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return movies.count
     }
     
-
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "MovieGridCell", for: indexPath) as! MovieGridCell
+        
+        
+        let movie = movies[indexPath.item]
+        
+        let baseUrl = "https://image.tmdb.org/t/p/w185"
+        let posterPath = movie["poster_path"] as! String
+        let posterUrl = URL(string: baseUrl + posterPath)!
+                
+        cell.posterView.af_setImage(withURL: posterUrl)
+        
+        return cell
+    }
+    
     /*
     // MARK: - Navigation
 
